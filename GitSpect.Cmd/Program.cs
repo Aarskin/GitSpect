fb1565897819dda664658d56c5e4addee8e1199f
aarskin <mwiemer@outlook.com>
2016-08-13 13:23:25 -0500

    isolated the powershell command calls; parsing full names now

--- GitSpect.Cmd/Program.cs
+++ GitSpect.Cmd/Program.cs
@@ -15,28 +15,61 @@ namespace GitSpect.Cmd
         static void Main(string[] args)
         {
             _graphDictionary = new Dictionary<string, GitObject>();
+            IEnumerable<PSObject> gitObjectHeaders;
 
-            using (PowerShell posh = PowerShell.Create())
+            // Get the first two letters of all the git objects 
+            // (also path and info, but we don't care about those yet)
+            string command = string.Format(@"cd {0}; ls", OBJECT_BASE);
+            gitObjectHeaders = ExecuteCommand(command);
+
+            foreach (var obj in gitObjectHeaders)
             {
-                string command = string.Format(@"cd {0}; ls", OBJECT_BASE);
-                posh.AddScript(command);
-
-                var results = posh.Invoke();
-
-                foreach (var result in results)
-                {
-                    ProcessGitObject(result);
-                    Console.WriteLine(result);
-                }
+                ProcessPSObject(obj);
+                Console.WriteLine(obj);
             }
 
             Console.WriteLine("Press any key to close this window");
             Console.ReadKey();
         }
 
-        private static void ProcessGitObject(PSObject firstTwoLetters)
+        private static GitObject ProcessPSObject(PSObject firstTwoLetters)
         {
-            
+            // Filter out pack and info
+            if(firstTwoLetters.ToString().Length == 2)
+            {
+                List<string> fullGitObjectNames = new List<string>();
+                IEnumerable<PSObject> results;
+                string command = string.Format(@"cd {0}; ls", firstTwoLetters);
+                results = ExecuteCommand(command);
+                
+                foreach (var result in results)
+                {
+                    string fullName = firstTwoLetters.ToString() + result.ToString();
+                    fullGitObjectNames.Add(fullName);
+                }
+            }
+
+            GitObject newObject = new Blob();
+
+            return newObject;
+        }
+
+        /// <summary>
+        /// Execute a command string using PowerShell, synchronously
+        /// </summary>
+        /// <param name="command"></param>
+        /// <returns>The PSObjects returned by the command invocation</returns>
+        public static IEnumerable<PSObject> ExecuteCommand(string command)
+        {
+            IEnumerable<PSObject> results;
+
+            using (PowerShell posh = PowerShell.Create())
+            {
+                posh.AddScript(command);
+                results = posh.Invoke();
+            }
+
+            return results;
         }
     }
 }
