a6dfaf06c8033fc6748ae6b47fb8d59f00d8a21d
aarskin <mwiemer@outlook.com>
2016-08-13 12:46:01 -0500

    running commands from C#

---
+++ GitSpect.Cmd/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitSpect.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (PowerShell posh = PowerShell.Create())
+            {
+                posh.AddScript(@"cd C:\Users\mwiem\OneDrive\Projects\GitSpect.Console\.git\objects; ls");
+
+                var results = posh.Invoke();
+
+                foreach (var result in results)
+                {
+                    System.Console.WriteLine(result);
+                }
+            }
+
+            System.Console.ReadKey();
+        }
+    }
+}
