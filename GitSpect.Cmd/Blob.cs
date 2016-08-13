9105ff19c8feaf63a7623aebba598fe95b13ba4c
aarskin <mwiemer@outlook.com>
2016-08-13 15:02:51 -0500

    added writerawblobtodisk

--- GitSpect.Cmd/Blob.cs
+++ GitSpect.Cmd/Blob.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Management.Automation;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +10,11 @@ namespace GitSpect.Cmd
 {
     class Blob : GitObject
     {
+        internal static void WriteRawBlobToDisk(PSObject[] catFileNiceResult)
+        {
+            string[] lines = catFileNiceResult.Select(x => x.ToString()).ToArray();
+            string path = string.Format(@"{0}\{1}");
+            File.AppendAllLines(path, lines);
+        }
     }
 }
