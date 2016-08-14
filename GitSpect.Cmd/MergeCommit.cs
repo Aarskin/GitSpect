fad1c86182c9d0420899b31e6646d08f76be721d
aarskin <mwiemer@outlook.com>
2016-08-14 16:25:21 -0500

    implemented mergecommit tostring

--- GitSpect.Cmd/MergeCommit.cs
+++ GitSpect.Cmd/MergeCommit.cs
@@ -1,4 +1,6 @@
-﻿namespace GitSpect.Cmd
+﻿using System.Text;
+
+namespace GitSpect.Cmd
 {
     internal class MergeCommit : Commit
     {
@@ -9,11 +11,25 @@
 
         public override string ToString()
         {
-            string prettyPrint;
+            string baseString = base.ToString();
+            StringBuilder prettyPrint = new StringBuilder(baseString);
 
+            string authorLine = string.Format("Author: {0}", Author);
+            string committerLine = string.Format("Committer: {0}", Committer);
+            string parentALine = string.Format("ParentA: {0}", ParentA);
+            string parentBLine = string.Format("ParentB: {0}", ParentB);
+            string treeLine = string.Format("Tree: {0}", Tree);
+            string messageLine = string.Format("Message: {0}", Message);
 
+            prettyPrint.AppendLine(authorLine);
+            prettyPrint.AppendLine(committerLine);
+            prettyPrint.AppendLine(parentALine);
+            prettyPrint.AppendLine(parentBLine);
+            prettyPrint.AppendLine(treeLine);
+            prettyPrint.AppendLine(messageLine);
+            prettyPrint.AppendLine();
 
-            return prettyPrint;
+            return prettyPrint.ToString();
         }
 
         public string ParentA { get; set; }
