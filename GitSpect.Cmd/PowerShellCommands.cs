365b5f2c4ee6f28ef633debd39738aec0d941b40
aarskin <mwiemer@outlook.com>
2016-08-14 13:02:20 -0500

    added powershell static class

---
+++ GitSpect.Cmd/PowerShellCommands.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitSpect.Cmd
+{
+    public class PowerShellCommands
+    {
+        public const string GET_LAST_15_MINUTES = "ls | where {$_.LastAccessTime -gt (Get-Date).AddMinutes(-35)}";
+    }
+}
