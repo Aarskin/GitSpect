e708b9c7de35f49c1079f55ac28ea9f641fd065f
aarskin <mwiemer@outlook.com>
2016-08-14 19:37:30 -0500

    Improved the tree tostring

--- GitSpect.Cmd/Tree.cs
+++ GitSpect.Cmd/Tree.cs
@@ -12,19 +12,23 @@ namespace GitSpect.Cmd
 
         public override string ToString()
         {
+            int i = 0;
             var baseString = base.ToString();
             StringBuilder prettyPrint = new StringBuilder(baseString);
 
             foreach (var blob in Blobs)
             {
-                string line = string.Format("Blob: {0}", blob.FileName);
+                string line = string.Format("B{0} - Blob: {1}", i, blob.FileName);
                 prettyPrint.AppendLine(line);
+                i++;
             }
 
+            i = 0;
             foreach (var tree in Trees)
             {
-                string line = string.Format("Tree: {0}", tree.FileName);
+                string line = string.Format("T{0} - Tree: {1}", i, tree.FileName);
                 prettyPrint.AppendLine(line);
+                i++;
             }
 
             prettyPrint.AppendLine();
